63cd98ee9c83593b7967b259af23a42e021b37f9
wanglufei <2686221966@qq.com>
2022-09-20 12:10:39 +0800

    初始化工程

---
+++ Hash/src/test/java/com/uin/hash/HashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uin.hash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Zset/src/main/java/com/uin/zset/ZsetApplication.java
@@ -0,0 +1,13 @@
+package com.uin.zset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZsetApplication.class, args);
+	}
+
+}
